e336c9683342407bb409b791f262d3196925c74f
程序员炒饭 <595409189@qq.com>
2020-08-28 15:06:02 +0800

    登录页面完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,7 @@
   <div>
     <hm_header>登录</hm_header>
     <hm_logo></hm_logo>
+    <!-- 登录组件 -->
     <van-form @submit="login">
       <van-field
         v-model="username"
@@ -16,10 +17,10 @@
         name="密码"
         label="密码"
         placeholder="密码"
-        :rules="rules.userpass"
+        :rules="rules.password"
       />
       <div style="margin: 16px;">
-        <van-button round block type="info" native-type="submit">
+        <van-button round block type="info" native-type="submit" color="skyblue">
           提交
         </van-button>
       </div>
@@ -27,6 +28,7 @@
   </div>
 </template>
 <script>
+// 下载的axios 在哪里用就在那里导入
 import axios from 'axios'
 export default {
   data() {
@@ -39,7 +41,7 @@ export default {
           { required: true, message: '请填写用户名', trigger: 'onChange' },
           { pattern: /^\d{5,11}$/, message: '用户名需在5到11位之间', trigger: 'onChange' }
         ],
-        userpass: [
+        password: [
           { required: true, message: '密码不能为空', trigger: 'onChange' },
           { pattern: /^\d{3,9}$/, message: '密码长度在3到9位之间', trigger: 'onChange' }
         ]
@@ -50,14 +52,17 @@ export default {
     async login() {
       const res = await axios.post('http://localhost:3000/login', {
         username: this.username,
-        password: this.userpass
+        password: this.password
       })
-      // console.log(res.data)
+      console.log(res.data)
+      // console.log(res.data.data.token);
+      // 对象结构 提取res对象中 statusCode message 的属性
       const { statusCode, message } = res.data
       if (statusCode === 200) {
-        alert(message)
+        this.$toast.success(message)
+        this.$router.push('/user')
       } else {
-        alert(message)
+        this.$toast(message)
       }
     }
   }
